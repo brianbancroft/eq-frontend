e129e2cd2f4c09cf6a733dc8dfa37d087f483c2b
Brian Bancroft <hello@brianbancroft.ca>
2018-05-01 12:18:09 -0400

    🚧  Base components

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Body extends Component {
+  render () {
+    return(
+      <div className="body">
+        hello body
+      </div>
+    );
+}
+
+}
+
+export default Body
---
+++ src/components/Sidenav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Sidenav extends Component {
+  render () {
+    return(
+      <div className="sidenav">
+        hello sidenav
+      </div>
+    )
+  }
+}
+
+export default Sidenav
