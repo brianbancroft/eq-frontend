6cd445016275334c5ef665d516f477d3d32e13af
Brian Bancroft <hello@brianbancroft.ca>
2018-05-01 14:54:56 -0400

    ➕  React for echarts

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import ReactEcharts from 'echarts-for-react'
 
 class Stats extends Component {
   render () {
     return(
       <div className="bar-chart">
         <h1>Bar Chart</h1>
+        <ReactEcharts option={ {foo: 'bar'}} />
       </div>
     )
   }
