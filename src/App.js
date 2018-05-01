20e1b9b4b9a458aa066368d0f7d1b46518097f1c
Brian Bancroft <hello@brianbancroft.ca>
2018-05-01 12:58:32 -0400

    ✨  ➕  React-router in main body

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react'
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { Grid, Row, Col } from 'react-bootstrap'
+
+import './App.css';
+
 import Body from './components/Body'
+import Events from './components/Events'
+import Stats from './components/Stats'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {selectedElement: Body}
+  }
+
+
   render() {
+
     return (
-      <Grid>
-        <Row>
-          <Col xs={4} md={3} className="side-nav">
-            Hello sidenav
-          </Col>
-          <Col xs={8} md={9}>
-            <Body />
-          </Col>
-        </Row>
-      </Grid>
+      <Router>
+        <Grid>
+          <Row>
+            <Col xs={4} md={3} className="side-nav">
+              <div className="site-title">
+                {this.state.selectedElement}
+              </div>
+
+              <div className="link-container">
+                <div className="link events-link">
+                  <Link to="/events">
+                    Events
+                  </Link>
+                </div>
+                <div className="link stats-link">
+                  <Link to="/stats">
+                    Stats
+                  </Link>
+                </div>
+              </div>
+            </Col>
+            <Col xs={8} md={9}>
+              <Route exact path="/" component={Body} />
+              <Route path="/events" component={Events} />
+              <Route path="/stats" component={Stats}  />
+            </Col>
+          </Row>
+        </Grid>
+      </Router>
     )
   }
 }
---
+++ src/components/Stats.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Stats extends Component {
+  render () {
+    return(
+      <div className="body">
+        hello stats
+      </div>
+    );
+}
+
+}
+
+export default Stats
