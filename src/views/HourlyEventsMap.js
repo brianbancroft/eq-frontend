4ff53424696d5bdc3dd405839bbf5440633492d7
Brian Bancroft <hello@brianbancroft.ca>
2018-05-02 15:23:59 -0400

    ✨  Render all points on the map

--- src/views/HourlyEventsMap.js
+++ src/views/HourlyEventsMap.js
@@ -1,14 +1,34 @@
 import React, { Component, } from 'react'
-import ReactMapboxGl, { Layer, Feature, } from 'react-mapbox-gl';
+import mapboxgl from 'mapbox-gl'
 import axios from 'axios'
 
-const Map = ReactMapboxGl({
-  accessToken: 'pk.eyJ1IjoiYnJpYW5iYW5jcm9mdCIsImEiOiJsVGVnMXFzIn0.7ldhVh3Ppsgv4lCYs65UdA',
-})
+
+const accessToken = 'pk.eyJ1IjoiYnJpYW5iYW5jcm9mdCIsImEiOiJsVGVnMXFzIn0.7ldhVh3Ppsgv4lCYs65UdA'
+const mapStyle = 'mapbox://styles/brianbancroft/cjgpfbonw002p2srogwwm99i2'
 
 class HourlyEventsMap extends Component {
+  componentDidMount () {
+    mapboxgl.accessToken = accessToken
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: mapStyle,
+      center: [-98.8067489, 49.0719174],
+      zoom: 3,
+    })
+  }
+
+  componentWillUnmount () {
+    this.map.remove()
+  }
+
   constructor (props) {
     super(props)
+    this.state = {
+      featureSet: {
+        type: 'FeatureCollection',
+        features: [],
+      },
+    }
 
     const buildGeojsonFeature = feat => ({
       type: 'Feature',
@@ -38,24 +58,70 @@ class HourlyEventsMap extends Component {
   }
 
   render () {
+    if (this.map) {
+      this.featureSet = this.state.featureSet
+      this.map.on('load', () => {
+        this.map.addLayer({
+          'id': 'points',
+          'type': 'circle',
+          'source': {
+            'type': 'geojson',
+            'data': this.state.featureSet,
+            // 'data': {
+            //   'type': 'FeatureCollection',
+            //   'features': [{
+            //     'type': 'Feature',
+            //     'geometry': {
+            //       'type': 'Point',
+            //       'coordinates': [-77.03238901390978, 38.913188059745586,],
+            //     },
+            //     'properties': {
+            //       'title': 'Mapbox DC',
+            //       'icon': 'monument',
+            //     },
+            //   }, {
+            //     'type': 'Feature',
+            //     'geometry': {
+            //       'type': 'Point',
+            //       'coordinates': [-122.414, 37.776,],
+            //     },
+            //     'properties': {
+            //       'title': 'Mapbox SF',
+            //       'icon': 'harbor',
+            //     },
+            //   },],
+            // },
+          },
+          // 'layout': {
+          //   'icon-image': '{icon}-15',
+          //   'text-field': '{title}',
+          //   'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold',],
+          //   'text-offset': [0, 0.6,],
+          //   'text-anchor': 'top',
+          // },
+          'paint': {
+            // make circles larger as the user zooms from z12 to z22
+            'circle-radius': {
+              'base': 1.75,
+              'stops': [[12, 12,], [22, 180,],],
+            },
+            // color circles by ethnicity, using a match expression
+            // https://www.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
+            'circle-color': '#3bb2d0',
+          },
+        })
+      })
+    }
+
+    const style = {
+      position: 'absolute',
+      height: '800px',
+      width: '800px',
+    }
+
     return (
       <div>
-        <Map
-          style='mapbox://styles/brianbancroft/cjgpfbonw002p2srogwwm99i2'
-          containerStyle={{
-            height: '800px',
-            width: '800px',
-            border: '2px solid #555',
-          }}>
-            <Layer
-              type='symbol'
-              id='marker'
-              layout={{ 'icon-image': 'marker-15', }}>
-              <Feature coordinates={
-                [-0.481747846041145, 51.3233379650232,]
-              }/>
-            </Layer>
-        </Map>
+        <div style={style} ref={el => this.mapContainer = el} />
       </div>
     )
   }
