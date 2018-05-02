457daa6b36bceaa8d2c7845c337515359d79b594
Brian Bancroft <hello@brianbancroft.ca>
2018-05-02 14:37:06 -0400

    ✨  Push features geojson into state

--- src/views/HourlyEventsMap.js
+++ src/views/HourlyEventsMap.js
@@ -1,12 +1,42 @@
 import React, { Component, } from 'react'
 import ReactMapboxGl, { Layer, Feature, } from 'react-mapbox-gl';
+import axios from 'axios'
 
 const Map = ReactMapboxGl({
   accessToken: 'pk.eyJ1IjoiYnJpYW5iYW5jcm9mdCIsImEiOiJsVGVnMXFzIn0.7ldhVh3Ppsgv4lCYs65UdA',
-
 })
 
 class HourlyEventsMap extends Component {
+  constructor (props) {
+    super(props)
+
+    const buildGeojsonFeature = feat => ({
+      type: 'Feature',
+      properties: {
+        date: feat.clean_date,
+        hour: feat.hour,
+        events: feat.events,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [feat.lon, feat.lat,],
+      },
+    })
+
+    const setupData = resp => {
+      this.setState({
+        featureSet: {
+          type: 'FeatureCollection',
+          features: resp.data.map(buildGeojsonFeature),
+        },
+      })
+    }
+
+    axios
+      .get('http://localhost:5555/events/hourly-map')
+      .then(setupData)
+  }
+
   render () {
     return (
       <div>
