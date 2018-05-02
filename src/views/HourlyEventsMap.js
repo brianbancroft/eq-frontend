e777469db963974296a4b4dc255fcbbf15471e82
Brian Bancroft <hello@brianbancroft.ca>
2018-05-02 15:34:45 -0400

    🔥  Hide content

--- src/views/HourlyEventsMap.js
+++ src/views/HourlyEventsMap.js
@@ -67,46 +67,13 @@ class HourlyEventsMap extends Component {
           'source': {
             'type': 'geojson',
             'data': this.state.featureSet,
-            // 'data': {
-            //   'type': 'FeatureCollection',
-            //   'features': [{
-            //     'type': 'Feature',
-            //     'geometry': {
-            //       'type': 'Point',
-            //       'coordinates': [-77.03238901390978, 38.913188059745586,],
-            //     },
-            //     'properties': {
-            //       'title': 'Mapbox DC',
-            //       'icon': 'monument',
-            //     },
-            //   }, {
-            //     'type': 'Feature',
-            //     'geometry': {
-            //       'type': 'Point',
-            //       'coordinates': [-122.414, 37.776,],
-            //     },
-            //     'properties': {
-            //       'title': 'Mapbox SF',
-            //       'icon': 'harbor',
-            //     },
-            //   },],
-            // },
           },
-          // 'layout': {
-          //   'icon-image': '{icon}-15',
-          //   'text-field': '{title}',
-          //   'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold',],
-          //   'text-offset': [0, 0.6,],
-          //   'text-anchor': 'top',
-          // },
           'paint': {
             // make circles larger as the user zooms from z12 to z22
             'circle-radius': {
               'base': 1.75,
               'stops': [[12, 12,], [22, 180,],],
             },
-            // color circles by ethnicity, using a match expression
-            // https://www.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
             'circle-color': '#3bb2d0',
           },
         })
